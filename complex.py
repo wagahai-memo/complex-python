ecf70af0915d3a427b6a4d73a39fed74fe58ae1f
masaru <mseik83@hotmail.co.jp>
2019-05-23 17:55:40 +0900

    Implement literal class method

--- complex.py
+++ complex.py
@@ -2,6 +2,33 @@
 
 
 class Complex:
+  element_type = None
+
+  @classmethod
+  def set_element_type(clazz, elem_type):
+    Complex.element_type = elem_type
+
+  @classmethod
+  def _0(clazz):
+    t = Complex.element_type
+    if t == int:
+      return Complex(0, 0)
+    elif t == float:
+      return Complex(0.0, 0.0)
+    else:
+      return Complex(t._0(), t._0())
+
+  @classmethod
+  def _1(clazz):
+    t = Complex.element_type
+    if t == int:
+      return Complex(1, 0)
+    elif t == float:
+      return Complex(1.0, 0.0)
+    else:
+      return Complex(t._1(), t._0())
+
+
   def __init__(self, real, imaginary):
     self.real = real
     self.imaginary = imaginary
@@ -13,5 +40,8 @@ class Complex:
 
 
 if __name__ == '__main__':
-  x = Complex(1, 2)
+  Complex.set_element_type(float)
+  print(Complex._0())
+  print(Complex._1())
+  x = Complex(1.0, 2.0)
   print(x)
